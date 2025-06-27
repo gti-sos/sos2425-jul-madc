4dcd1f833553477e7e217bd701409e1ecf786c41
Miguel Ángel <migdomcie@alum.us.es>
2025-06-27 13:31:18 +0200

    modificado svelteconfig, viteconfig y añadido mas memoria a build

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -1,14 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter({
-      out: 'build',
-      precompress: false,
-      envPrefix: ''
-    })
-  }
-};
+const config = { kit: { adapter: adapter() } };
 
 export default config;
--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -1,25 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 console.log("SE esta ejecutando o no el vite!!!!!!!!");
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0', // Requerido por Render
-		port: Number(process.env.PORT) || 5173, // Usa el puerto dinámico asignado por Render
-		fs: {
-			allow: [
-				path.resolve(__dirname, '.'),
-				path.resolve(__dirname, '../..')
-			]
-		},
-		proxy: {
-			'/temp-api': {
-				target: 'https://sos2425-15.onrender.com',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/temp-api/, '/api/v1/temperature-stats')
-			}
-		}
-	}
+	plugins: [sveltekit()]
 });
