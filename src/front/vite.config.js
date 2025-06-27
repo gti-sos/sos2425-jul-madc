9ce95444e64384ae3fe32e68024fbf6b0877e374
Miguel Ángel <migdomcie@alum.us.es>
2025-06-27 14:39:01 +0200

    modificado viteconfig

--- src/front/vite.config.js
+++ src/front/vite.config.js
@@ -1,7 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 console.log("SE esta ejecutando o no el vite!!!!!!!!");
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0', // Requerido por Render
+		port: Number(process.env.PORT) || 5173, // Usa el puerto dinámico asignado por Render
+		fs: {
+			allow: [
+				path.resolve(__dirname, '.'),
+				path.resolve(__dirname, '../..')
+			]
+		}
+	}
 });
